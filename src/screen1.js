cc6bf7aaea4aeaf8a66d3772defa0c3a82a6943c
Rashila Rathnasiri <rashila.rathnasiri@techsys.lk>
2019-04-02 13:06:43 +0530

    error is coming from save button

--- src/screen1.js
+++ src/screen1.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput, KeyboardAvoidingView, Text } from 'react-native';
+import { StyleSheet, View, TextInput, KeyboardAvoidingView, Text, Alert } from 'react-native';
+import Button from './Button';
 
 class screen1 extends Component {
     static navigationOptions = {
-      title: 'Edit User Details',
-    //   headerTitleStyle: { 
-    //     textAlign:"center", 
-    //     flex:1
-    // }
+      title: 'Edit User Details'
     };
 
     constructor(props){
+      
       super(props);
       this.state = {
         id: this.props.navigation.state.params.id,
@@ -21,30 +19,56 @@ class screen1 extends Component {
         
       }
     }
+
+    onButtonPress(){
+
+      fetch('http://192.168.2.23:100/api/employee/save/')
+      .then(response => response.json())
+      .then((responseJson) => {
+        Alert.alert(responseJson);
+    }).catch((error) => {
+        Alert.alert(error);
+    });
+    }
+
+    renderButton() {
+      return (
+          <Button onPress={this.onButtonPress.bind(this)}>
+          SAVE
+        </Button>
+      );
+
+}
+
     render() {
+      console.log("id");
       const { navigate } = this.props.navigation;
       return (
-        <KeyboardAvoidingView>
+        
         <View style={styles.container}>
 
         <Text>hello</Text>
-        <Text>{this.state.id}</Text>
-        <Text>{this.state.firstName}</Text>
-        <Text>{this.state.lastName}</Text>
-        <Text>{this.state.email}</Text>
-        <Text>{this.state.displayName}</Text>
 
         <TextInput
-          placeholder={this.state.firstName}
-          onChangeText={firstName => this.setState({ firstName })}
-        ></TextInput>
-        
-        
-        
+        value= {this.state.id}
+        onChangeText={id => this.setState({ id })}/>
 
+        <TextInput>{this.state.firstName}</TextInput>
+        <TextInput>{this.state.lastName}</TextInput>
+        <TextInput>{this.state.email}</TextInput>
+        <TextInput>{this.state.displayName}</TextInput>
+
+        <View style={styles.buttonStyle}>
+        {this.renderButton()}
         </View>
-        </KeyboardAvoidingView>
+
+
+        
+        </View>
+      
       );
+
+      
     }
   }
 
@@ -55,7 +79,15 @@ const styles = StyleSheet.create({
         paddingLeft: 250,
         flex: 1,
         backgroundColor: '#F5FCFF'
-    }
+    },
+    buttonStyle: {
+    marginTop: 5,
+    padding: 5,
+    borderRadius: 60,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    position: 'relative',
+}
     
 })
 export default screen1 ;
